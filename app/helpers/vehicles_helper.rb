9e278e410a473ec0c261759c5537ed33cb41f994
Buddy Magsipoc <keikun17@gmail.com>
2014-05-01 11:11:09 +0800

    Remove comma causing error。

--- app/helpers/vehicles_helper.rb
+++ app/helpers/vehicles_helper.rb
@@ -1,11 +1,11 @@
 module VehiclesHelper
 
   def vehicle_state(vehicle)
-    content_tag :span, do
+    content_tag :span do
       "(#{vehicle.state})"
     end
   end
-  
+
   def mileage_reading(vehicle)
     reading = vehicle.mileages.empty? ? 0.00 : round_off(vehicle.mileages.last.reading,2)
   end
